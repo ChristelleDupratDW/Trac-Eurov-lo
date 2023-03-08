2425b8ec08db277f7afe3a94c377cef64bee06a7
ChristelleDupratDW <121934272+ChristelleDupratDW@users.noreply.github.com>
2023-03-08 07:28:56 +0100

    Add files via upload

---
+++ TracéEurovélo/don-lille.js
@@ -0,0 +1,26 @@
+
+var map = L.map('map').setView([50.9651981, 1.8630496], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+var gpx = 'Troncons/don-lille.gpx'; // URL to your GPX file or the GPX itself
+new L.GPX(gpx, {async: true}).on('loaded', function(e) {
+  map.fitBounds(e.target.getBounds());
+  
+}).addTo(map);
+
+
+    
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+
+
+map.on('click', onMapClick);
